881f3a3fefc79b4a9dc5b42916be19a756533943
Sjaak Priester <sjaak@sjaakpriester.nl>
2019-12-09 15:55:09 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # yii2-random-provider
- ActiveDataProvider with random selection
+
+#### ActiveDataProvider with random selection ####
+
+[![Latest Stable Version](https://poser.pugx.org/sjaakp/yii2-random-provider/v/stable)](https://packagist.org/packages/sjaakp/yii2-random-provider)
+[![Total Downloads](https://poser.pugx.org/sjaakp/yii2-random-provider/downloads)](https://packagist.org/packages/sjaakp/yii2-random-provider)
+[![License](https://poser.pugx.org/sjaakp/yii2-random-provider/license)](https://packagist.org/packages/sjaakp/yii2-random-provider)
+
+**RandomProvider** is derived from [**ActiveDataProvider**](https://www.yiiframework.com/doc/api/2.0/yii-data-activedataprovider)
+of the [Yii 2.0](https://www.yiiframework.com/ "Yii") PHP Framework. It selects the records in a random
+fashion, which in some cases may be more attractive than the orderly way a regular
+**ActiveDataProvider** (usually) does it. **RandomProvider** is intended to co-operate with my
+[**LoadMorePager**](https://sjaakpriester.nl/software/loadmore), but it will work with
+[**LinkPager**](https://www.yiiframework.com/doc/api/2.0/yii-widgets-linkpager) or other pagers as well.
+
+Notice that **RandomProvider** doesn't support `CUBRID` or `dblib` database drivers. Moreover, 
+I only tested it with `mysql`. I'm pretty sure it'll work with other drivers, though.
+If you  have any experiences to share, I'll appreciate that.
+
+Notice also that **RandomProvider** makes use of an algorithm named '*Order By Rand()*'. This is
+rather slow, and doesn't scale very well. Therefore, it is advised to use **RandomProvider** only with
+relatively small data sets (think of less than a few thousands of records).
+More information [here](https://www.warpconduit.net/2011/03/23/selecting-a-random-record-using-mysql-benchmark-results/).
+ 
+A demonstration of **RandomProvider** is [here](http://www.sjaakpriester.nl/software/randomprovider).
+
+## Installation ##
+
+Install **yii2-random-provider** in the usual way with [Composer](https://getcomposer.org/). 
+Add the following to the `require` section of your `composer.json` file:
+
+`"sjaakp/yii2-random-provider": "*"` 
+
+or run:
+
+`composer require sjaakp/yii2-random-provider` 
+
+You can manually install **yii2-random-provider** by [downloading the source in ZIP-format](https://github.com/sjaakp/yii2-random-provider/archive/master.zip).
+
+## Using RandomProvider ##
+
+**RandomProvider** is a drop-in replacement for Yii's
+[**ActiveDataProvider**](https://www.yiiframework.com/doc/api/2.0/yii-data-activedataprovider).
+Just use it like **ActiveDataProvider**.
---
+++ RandomProvider.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * MIT licence
+ * Version 1.0
+ * Sjaak Priester, Amsterdam 09-12-2019.
+ *
+ * RandomProvider - unordered data-provider for Yii 2.0
+ */
+
+namespace sjaakp\randomprovider;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\data\ActiveDataProvider;
+use yii\db\Expression;
+use yii\helpers\Json;
+
+/**
+ * Class RandomProvider
+ * @package sjaakp\randomprovider
+ * @link https://www.warpconduit.net/2011/03/23/selecting-a-random-record-using-mysql-benchmark-results/
+ * Notice that this may not work properly if the model has multiple primary key.
+ */
+class RandomProvider extends ActiveDataProvider
+{
+    /**
+     * @var string|null  name under which RandomProvider's information is stored in session
+     * if null: auto-generated
+     */
+    public $sessionKey;
+
+    /**
+     * @var bool  whether RandomProvider resets itself when on the first page
+     */
+    public $autoReset = true;
+
+    private $_randCmd;
+
+    /**
+     * @inheritDoc
+     */
+    public function init()
+    {
+        // @link https://stackoverflow.com/questions/580639/how-to-randomly-select-rows-in-sql
+        // Notice that this is only tested with mysql!
+        $commands = [
+            'mysql' => 'RAND()',
+            'mysqli' => 'RAND()',
+            'mssql' => 'NEWID()',
+            'oci' => 'dbms_random.value',
+            'pgsql' => 'RANDOM()',
+            'sqlite' => 'RANDOM()',
+            'sqlite2' => 'RANDOM()',
+            'sqlsrv' => 'NEWID()',
+        ];
+        parent::init();
+        $conn = $this->db ?? Yii::$app->db;
+        $driver = $conn->driverName;
+        $this->_randCmd = $commands[$driver] ?? null;
+
+        if (is_null($this->_randCmd))  {
+            $cls = __CLASS__;
+            throw new InvalidConfigException("$cls: driver \"$driver\" is not supported.");
+        }
+
+        $this->setSort(false);
+        if (is_null($this->sessionKey)) $this->sessionKey = '_rnd_' . $this->id;
+    }
+
+    /**
+     * @inheritDoc
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function prepareModels()
+    {
+        /** @var $query yii\db\ActiveQuery */
+        /** @var $mc yii\db\ActiveRecord */
+        $query = clone $this->query;
+
+        $mc = $query->modelClass;
+        $pId = current($mc::primaryKey());
+        $session = Yii::$app->session;
+        $visited = [];
+        $pagination = $this->pagination;
+        if ($pagination !== false) {
+            $pagination->totalCount = $this->getTotalCount();
+            if ($pagination->totalCount === 0) {
+                return [];
+            }
+            $page = $pagination->page;
+            if ($page === 0 && $this->autoReset)  {
+                $session->remove($this->sessionKey);
+            }
+            $visited = Json::decode($session->get($this->sessionKey, '[]'));
+            $key = 'p' . $page;
+            if (isset($visited[$key])) {   // this page was previously visited, retrieve the same records
+                // Notice that the order of the records will probably be different
+                return $query->where([ 'in', $pId, $visited[$key] ])->orderBy('')->all($this->db);
+            }
+            $limit = $pagination->pageSize;
+            if ($page >= ($pagination->pageCount - 1)) $limit = $pagination->totalCount % $limit;
+            $query->limit($limit);
+            if (count($visited))   {   // select records not previously selected
+                $query->andWhere([
+                    'not in', $pId, call_user_func_array("array_merge", $visited)
+                ]);
+            }
+        }
+        $query->orderBy(new Expression($this->_randCmd));
+        $r = $query->all($this->db);
+        if ($pagination !== false)   {
+            $newSel = array_column($r, $pId);
+            $visited[$key] = $newSel; // add to visited pages
+            $session->set($this->sessionKey, Json::encode($visited));
+        }
+        return $r;
+    }
+}
